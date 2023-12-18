50f2841cb1089f0d8732db446e220b1433eaf0da
unknown <ketanprajapati0267@gmail.com>
2023-12-18 12:27:25 +0530

    first commit

---
+++ src/Component/pls.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const pls = props => {
+  return (
+    <div>
+      <h1> Ketan Prajapati</h1>
+    
+    </div>
+  )
+}
+
+pls.propTypes = {
+    constructer()
+
+}
+
+export default pls
